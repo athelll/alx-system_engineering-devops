cd24d3838908b11e75c2ddd29a517735784c53a6
felixhatesbugs <felix.okoronkwo.1@gmail.com>
2023-10-04 09:45:48 +0100

    deleted directories

--- 0x16-api_advanced/0-subs.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""gets the number of subscribers from a subreddit"""
-from requests import get
-
-
-def number_of_subscribers(subreddit):
-    about = get('https://reddit.com/r/{}/about.json'.format(subreddit),
-                headers={'User-Agent': 'felixhatesbugs'})
-    return about.json().get('data', {}).get('subscribers', 0)
--- 0x18-webstack_monitoring/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-DD_SITE="datadoghq.com" DD_API_KEY="$1" DD_APP_KEY="$2" python3 $3
